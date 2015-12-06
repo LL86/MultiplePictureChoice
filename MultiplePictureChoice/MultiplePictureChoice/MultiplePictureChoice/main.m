eea6ab1d12375ab7566e491c310f75d6d38d9d3e
LL <18657141238@163.com>
2015-12-06 16:30:24 +0800

    project init

---
+++ MultiplePictureChoice/MultiplePictureChoice/MultiplePictureChoice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MultiplePictureChoice
+//
+//  Created by 史练练 on 15/12/6.
+//  Copyright © 2015年 史练练. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
